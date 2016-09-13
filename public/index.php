89c57eb573ee55c622ff60fac35ebb2903a497aa
Eric Ehmann <eaehmann@gmail.com>
2016-09-13 14:47:27 -0400

    updated html and css page

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Portfolio Displayer</title>
+</head>
+<body>
+<h1>Eric Ehmann</h1>
+<h2> Portfolio Displayer</h2>
+<img src="picture.jpg" alt="Picture of Eric Ehmann" />
+<p>Hello! My name is Eric Ehmann.  And I am philantrhopist turned developer.  I have founded a theater program for at-risk-youth in Hawaii, delivered uplifting sermons, trained people in leadership and public speaking, inspired people to become clean, empowered people on course to finish marathons, and raised over $10K for cancer research.  The secret to improving lives, is to meet people where they are and understand how they think, to create better solutions.  Because people spend so much time online, I am transition to becoming a developer.  Below is a sample of my work.</p>
+<ul>
+	<li> Password Generator</li>
+    <ul> 
+    	<li><a href ="#">Live Demo </a></li>
+        <li><a href="#">Github</a></li>
+    </ul>
+    <li> Random Text Generator and Random User Generator</li>
+    <ul> 
+    	<li><a href="#">Live Demo</a> </li>
+        <li><a href="#">Github</a></li>
+    </ul>
+    <li> Most exciting project</li>
+    <ul> 
+    	<li><a href="#">Live Demo</a></li>
+        <li><a href="#">Github</a></li>
+    </ul>
+    
+</body>
+</html>
