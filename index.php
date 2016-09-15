9862833ca1165fdb302dafec959c356ad7860a10
Eric Ehmann <eaehmann@gmail.com>
2016-09-15 06:28:11 -0400

    changes in section space, closing a list and editing line items

--- index.php
+++ index.php
@@ -6,35 +6,42 @@
 <title>Portfolio Displayer</title>
 </head>
 <body>
-<h1>Eric Ehmann</h1>
-<h2> Portfolio Displayer</h2>
-<img src="picture.jpg" alt="Picture of Eric Ehmann"  />
+<main>
+    <h1>Eric Ehmann</h1>
+    <h2> Portfolio Displayer</h2>
+    <img src="picture.jpg" alt="Picture of Eric Ehmann"  />
 
 
-<h3> Examples of Work</h3>
-<ul>
-	<li> Project 2 XABC Password Generator</li>
-    <ul> 
-        <li> A tool generates easy to remember but hard to crack passwords</li>
-    	<li><a href ="#">Live Demo </a></li>
-        <li><a href="#">Github</a></li>
+    <h3> Examples of Work</h3>
+    <ul>
+	   <li> Project 2 XABC Password Generator</li>
+        <ul> 
+            <li> A tool that generates easy to remember but hard to crack passwords</li>
+    	   <li><a href ="#">Live </a></li>
+            <li><a href="#">Github</a></li>
+            <li><a href=#>Video Demo</a></li>
+        </ul>
+        <li> Project 3 Developer's Friend</li>
+        <ul> 
+            <li> A tool that generates random text or builds a random user profile</li>
+    	   <li><a href="#">Live</a> </li>
+            <li><a href="#">Github</a></li>
+            <li><a href=#>Video Demo</a></li>
+        </ul>
+        <li> Project 4 Most exciting project</li>
+        <ul> 
+            <li> This is just something that you need to see</li>
+    	   <li><a href="#">Live Site</a></li>
+            <li><a href="#">Github</a></li>
+            <li><a href=#>Video Demo</a></li>
+        </ul>
     </ul>
-    <li> Project 3 Developer's Friend</li>
-    <ul> 
-        <li> A tool that generates random text or builds a random user profile</li>
-    	<li><a href="#">Live Demo</a> </li>
-        <li><a href="#">Github</a></li>
-    </ul>
-    <li> Project 4 Most exciting project</li>
-    <ul> 
-        <li> This is just something that you need to see</li>
-    	<li><a href="#">Live Demo</a></li>
-        <li><a href="#">Github</a></li>
-    </ul>
+</main>
 <footer>
-<p>Eric Ehmann is a designer residing in the Boston area.  He is concerned about the mental shaming people do to themselves when the website is not intuitive to their way of thinking.  Ultimately he is interested interested in building websites and web applications that empower users.</p>
-<p> Eric considers his web design to be a continuation of the philanthropic work that he does.  His other philanthropic work includes founding a theater for at risk youth in Hawaii, training people on leadership and public speaking, inspiring people to become clean and sober, empowering people to finish their first marathons, and raising $10K for researching cures for blood cancer</p>
-<p>He is currently enrolled at Harvard Extension School, where he plans to earn a certificate in Web Technologies by the end of 2016 and a Masters degree in Digital Media Design by the end of 2017.  Additionally, he is certified public speaker and has earned an AS in Sociology, a BA in Humanities, a MBA in Marketing, and has studied Psychology.  He loves studying people and designing solutions to their problems.</p>
+    <h3>About Me<h3>
+    <p>Eric Ehmann is a designer residing in the Boston area.  He is concerned about the mental shaming people do to themselves when the website is not intuitive to their way of thinking.  Ultimately he is interested interested in building websites and web applications that empower users.</p>
+    <p> Eric considers his web design to be a continuation of the philanthropic work that he does.  His other philanthropic work includes founding a theater for at risk youth in Hawaii, training people on leadership and public speaking, inspiring people to become clean and sober, empowering people to finish their first marathons, and raising $10K for researching cures for blood cancer</p>
+    <p>He is currently enrolled at Harvard Extension School, where he plans to earn a certificate in Web Technologies by the end of 2016 and a Masters degree in Digital Media Design by the end of 2017.  Additionally, he is certified public speaker and has earned an AS in Sociology, a BA in Humanities, a MBA in Marketing, and has studied Psychology.  He loves studying people and designing solutions to their problems.</p>
 </footer>
 </body>
 </html>
