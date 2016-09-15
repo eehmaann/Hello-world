0c2c41d6e99157cf58abca370473e98f18180dca
Eric Ehmann <eaehmann@gmail.com>
2016-09-14 22:01:15 -0400

    Reorganizing page

--- index.php
+++ index.php
@@ -9,9 +9,7 @@
 <h1>Eric Ehmann</h1>
 <h2> Portfolio Displayer</h2>
 <img src="picture.jpg" alt="Picture of Eric Ehmann"  />
-<p>Eric Ehmann is a designer residing in the Boston area.  He is concerned about the mental shaming people do to themselves when the website is not intuitive to their way of thinking.  Ultimately he is interested interested in building websites and web applications that empower users. and examples of his work can be seen below</p>
-<p> Eric considers his web design to be a continuation of the philanthropic work that he does.  His other philanthropic work includes founding a theater for at risk youth in Hawaii, training people on leadership and public speaking, inspiring people to become clean and sober, empowering people to finish their first marathons, and raising $10K for researching cures for blood cancer</p>
-<p>He is currently enrolled at Harvard Extension School, where he plans to earn a certificate in Web Technologies by the end of 2016 and a Masters degree in Digital Media Design by the end of 2017.  Additionally, he is certified public speaker and has earned an AS in Sociology, a BA in Humanities, a MBA in Marketing, and has studied Psychology.  He loves studying people and designing solutions to their problems.</p>
+
 
 <h3> Examples of Work</h3>
 <ul>
@@ -33,6 +31,10 @@
     	<li><a href="#">Live Demo</a></li>
         <li><a href="#">Github</a></li>
     </ul>
-    
+<footer>
+<p>Eric Ehmann is a designer residing in the Boston area.  He is concerned about the mental shaming people do to themselves when the website is not intuitive to their way of thinking.  Ultimately he is interested interested in building websites and web applications that empower users.</p>
+<p> Eric considers his web design to be a continuation of the philanthropic work that he does.  His other philanthropic work includes founding a theater for at risk youth in Hawaii, training people on leadership and public speaking, inspiring people to become clean and sober, empowering people to finish their first marathons, and raising $10K for researching cures for blood cancer</p>
+<p>He is currently enrolled at Harvard Extension School, where he plans to earn a certificate in Web Technologies by the end of 2016 and a Masters degree in Digital Media Design by the end of 2017.  Additionally, he is certified public speaker and has earned an AS in Sociology, a BA in Humanities, a MBA in Marketing, and has studied Psychology.  He loves studying people and designing solutions to their problems.</p>
+</footer>
 </body>
 </html>
