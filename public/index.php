40e08d1487cef8710ed88a0ce00ca2da4780cc25
Eric Ehmann <eaehmann@gmail.com>
2016-09-14 21:55:08 -0400

    updating content

--- public/index.php
+++ public/index.php
@@ -9,21 +9,28 @@
 <h1>Eric Ehmann</h1>
 <h2> Portfolio Displayer</h2>
 <img src="picture.jpg" alt="Picture of Eric Ehmann"  />
-<p>Hello! My name is Eric Ehmann, and I am philantrhopist turned developer.  I have founded a theater program for at-risk-youth in Hawaii, trained people in leadership and public speaking, inspired people to become clean, empowered people to finish marathons, and have raised over $10K for cancer research.  The secret to improving lives, is to meet people where they are and understand how they think, to create better solutions.  Therefore, I look for how to use this ambition to create wesbites and web applications, and this is where people spend a large portion of their lives.  Below is a sample of my work.</p>
+<p>Eric Ehmann is a designer residing in the Boston area.  He is concerned about the mental shaming people do to themselves when the website is not intuitive to their way of thinking.  Ultimately he is interested interested in building websites and web applications that empower users. and examples of his work can be seen below</p>
+<p> Eric considers his web design to be a continuation of the philanthropic work that he does.  His other philanthropic work includes founding a theater for at risk youth in Hawaii, training people on leadership and public speaking, inspiring people to become clean and sober, empowering people to finish their first marathons, and raising $10K for researching cures for blood cancer</p>
+<p>He is currently enrolled at Harvard Extension School, where he plans to earn a certificate in Web Technologies by the end of 2016 and a Masters degree in Digital Media Design by the end of 2017.  Additionally, he is certified public speaker and has earned an AS in Sociology, a BA in Humanities, a MBA in Marketing, and has studied Psychology.  He loves studying people and designing solutions to their problems.</p>
+
+<h3> Examples of Work</h3>
 <ul>
-	<li> Project 2 Password Generator</li>
+    <li> Project 2 XABC Password Generator</li>
     <ul> 
-    	<li><a href ="#">Live Demo </a></li>
+        <li> A tool generates easy to remember but hard to crack passwords</li>
+        <li><a href ="#">Live Demo </a></li>
         <li><a href="#">Github</a></li>
     </ul>
-    <li> Project 3 Random Text Generator and Random User Generator</li>
+    <li> Project 3 Developer's Friend</li>
     <ul> 
-    	<li><a href="#">Live Demo</a> </li>
+        <li> A tool that generates random text or builds a random user profile</li>
+        <li><a href="#">Live Demo</a> </li>
         <li><a href="#">Github</a></li>
     </ul>
     <li> Project 4 Most exciting project</li>
     <ul> 
-    	<li><a href="#">Live Demo</a></li>
+        <li> This is just something that you need to see</li>
+        <li><a href="#">Live Demo</a></li>
         <li><a href="#">Github</a></li>
     </ul>
     
